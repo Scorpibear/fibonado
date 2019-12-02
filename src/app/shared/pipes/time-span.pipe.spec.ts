e55340cd9cd9fc41ba3729db3b9fc7d1eca328cf
Mikhail Subach <scorpibear@tut.by>
2019-12-02 12:56:12 +0300

    timeSpan pipe added

--- src/app/shared/pipes/time-span.pipe.spec.ts
+++ src/app/shared/pipes/time-span.pipe.spec.ts
@@ -1,8 +1,18 @@
 import { TimeSpanPipe } from './time-span.pipe';
 
 describe('TimeSpanPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeSpanPipe();
-    expect(pipe).toBeTruthy();
-  });
+  const pipe = new TimeSpanPipe();
+
+  it('works for just seconds', () => {
+    expect(pipe.transform(1000)).toEqual("0:01");
+  })
+  it('works for one digit minute', () => {
+    expect(pipe.transform(5 * 60 * 1000)).toEqual("5:00");
+  })
+  it('works for one digit minute with hours', () => {
+    expect(pipe.transform(65 * 60 * 1000)).toEqual("1:05:00");
+  })
+  it('works for days', () => {
+    expect(pipe.transform(25 * 60 * 60 * 1000)).toEqual("1d1:00:00");
+  })
 });
--- src/app/shared/pipes/time-span.pipe.ts
+++ src/app/shared/pipes/time-span.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeSpanPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(milliseconds: number): string {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+    const dd = Math.floor(totalHours / 24);
+    const ss = totalSeconds % 60;
+    const mm = totalMinutes % 60;
+    const hh = totalHours % 24;
+    return `${dd > 0 ? `${dd}d` : ''}${totalHours > 0 ? `${hh}:` : ''}${totalHours > 0 ? this.toLeadZero(mm) : mm}:${this.toLeadZero(ss)}`;
+  }
+
+  toLeadZero(value: number): string {
+    return value.toString().padStart(2, '0');
   }
 
 }
