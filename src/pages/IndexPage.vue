d8ac50104d9e8808aed2239d5b844c18a27f5681
Mikhail Subach <mikhail_subach@epam.com>
2025-01-31 13:54:43 +0300

    init version

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,66 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="q-pa-md">
+      <h2>Task Timer</h2>
+      <div>
+        <q-btn color="primary" label="Start" @click="startTask" />
+        <q-btn color="secondary" label="Continue" @click="continueTask" />
+        <q-btn color="accent" label="Next" @click="nextTask" />
+        <q-btn color="warning" label="Break" @click="takeBreak" />
+      </div>
+      <div class="q-mt-md">
+        <p>Total Time Spent: {{ totalTime }} seconds</p>
+        <p>Time Remaining: {{ timeRemaining }} seconds</p>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup>
-//
+<script>
+export default {
+  data() {
+    return {
+      totalTime: 0,
+      timeRemaining: 1500, // Example: 25 minutes in seconds
+      timer: null,
+    }
+  },
+  methods: {
+    startTask() {
+      this.resetTimer()
+      this.timer = setInterval(() => {
+        if (this.timeRemaining > 0) {
+          this.timeRemaining--
+          this.totalTime++
+        } else {
+          clearInterval(this.timer)
+        }
+      }, 1000)
+    },
+    continueTask() {
+      // Logic to continue the task can be added here
+      this.startTask()
+    },
+    nextTask() {
+      // Logic for moving to the next task can be added here
+      this.resetTimer()
+    },
+    takeBreak() {
+      // Logic for taking a break can be added here
+      clearInterval(this.timer)
+    },
+    resetTimer() {
+      this.totalTime = 0
+      this.timeRemaining = 1500 // Resetting to initial session time
+      clearInterval(this.timer)
+    },
+  },
+}
 </script>
+
+<style scoped>
+.q-pa-md {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
