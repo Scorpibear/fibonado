ec5d6a69ab262c09158fb742f95dd515cb029fcd
Mikhail Subach <mikhail_subach@epam.com>
2026-04-18 23:02:59 +0300

    debug mode false

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -28,7 +28,7 @@
 import { ref, computed } from 'vue'
 import { Notify } from 'quasar'
 
-const debugMode = true
+const debugMode = false
 
 const getLocalTime = (date) =>
   new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }).format(
